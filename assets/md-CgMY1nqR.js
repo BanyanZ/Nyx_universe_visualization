60207388fc30cc043af459a2f565aab13ed90dde
BanyanZ <2858394912@qq.com>
2026-06-12 23:44:29 +0800

    Tune transparency and foreground galaxy clicks

--- assets/md-CgMY1nqR.js
+++ assets/md-CgMY1nqR.js
@@ -1,4 +1,4 @@
-import{t as F,Q as W,S as r,h as p,e,f as B,aa as h,a5 as m,aF as P,ay as K,G as V,F as z,Z as j,I as U,m as g,ax as tt,k as et,l as T,Y as k,c as i,aE as at,H as st,s as lt}from"./modules/vue-B3mVpOOr.js";import{u as Z,f as nt}from"./slidev/context-DPmZjksc.js";import{a as ot}from"./index-BFQYi-2g.js";import{I as it}from"./slidev/default-DXQg7nRJ.js";import"./modules/shiki-DFdCPIZE.js";/**
+import{t as F,Q as W,S as r,h as p,e,f as B,aa as h,a5 as m,aF as P,ay as K,G as V,F as z,Z as j,I as U,m as g,ax as tt,k as et,l as T,Y as k,c as i,aE as at,H as st,s as lt}from"./modules/vue-B3mVpOOr.js";import{u as Z,f as nt}from"./slidev/context-B2BQ7bIy.js";import{a as ot}from"./index-CFqXX7HO.js";import{I as it}from"./slidev/default-DYvl98Qx.js";import"./modules/shiki-DFdCPIZE.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
